a991e5d95d3e97b14b12647e8ef5194ecf80a84f
goncy <gonzalo.pozzo4@gmail.com>
2018-07-09 17:38:21 -0300

    Adds grids gap for grid

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export default styled.div`
   display: grid;
+  grid-gap: 12px;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   width: 100%;
 `;
